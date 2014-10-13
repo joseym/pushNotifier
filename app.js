ee619f6ed92ec63f9068c8d35e5921efdc0b23e2
Josey Morton <me@joseymorton.com>
2014-10-12 20:28:04 -0500

    another local deploy test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Push = require('./push.js')
 
 function clearScreen(){ process.stdout.write('\033c'); }
 
-console.log('test');
+console.log('another test');
 
 // var q = 'from:alliebaldridge@gmail.com is:unread';
 var q = 'from:(alliebaldridge@gmail.com OR jmorton@mortlabs.com) is:unread';
