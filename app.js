aedec4a10649a147c88d6bfeabde6e233240e954
Josey Morton <me@joseymorton.com>
2014-10-12 22:41:45 -0500

    ...

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Push = require('./push.js')
 
 function clearScreen(){ process.stdout.write('\033c'); }
 
-console.log('<middle-fucking-finger />');
+console.log('<middle-fucking-finger times=2 />');
 
 // var q = 'from:alliebaldridge@gmail.com is:unread';
 var q = 'from:(alliebaldridge@gmail.com OR jmorton@mortlabs.com) is:unread';
